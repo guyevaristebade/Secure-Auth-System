b358811ea0f44f436ec5c2c725c9e55310232a5c
guyevaristebade <guy.badepro@gmail.com>
2025-07-24 21:14:49 +0200

    j'ai testé quelque chose avec zod

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,9 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/error.middleware';
 import { authRouter } from './routes/auth.route';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 app.use('/api/auth', authRouter);
 
 app.use(errorHandler);
--- server/src/middlewares/error.middleware.ts
+++ server/src/middlewares/error.middleware.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import { ZodError } from 'zod';
+
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     console.log(err.stack);
 
+    // gestion des erreurs lié à zod
+    // if (err instanceof ZodError) {
+    //     return res.status(400).json({ error: 'Impossible de valider les données' });
+    // }
+
     const status = err.status || 500;
-    const message = err.message || 'Erreur server';
+    const message = err.message || 'Erreur interne du serveur';
     res.status(status).json({ error: message });
 };
