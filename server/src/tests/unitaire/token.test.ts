3b94593294c899c1c3428a369a5395be8450099d
guyevaristebade <guy.badepro@gmail.com>
2025-08-02 00:22:32 +0200

    refactor(test) : mise à jour des tests

--- server/src/tests/unitaire/token.test.ts
+++ server/src/tests/unitaire/token.test.ts
@@ -1,34 +1,40 @@
-import { generateAccessToken, generateRefreshToken } from '../../helpers';
 import jwt from 'jsonwebtoken';
+import { UserPayload } from '../../types';
+import { generateAccessToken, generateRefreshToken } from '../../helpers';
 
 describe('Helpers de gestion des tokens', () => {
-    const userId = 'user123';
+    const userPayload: UserPayload = {
+        id: 'user123',
+        email: 'test@example.com',
+        name: 'Jules',
+        role: 'USER',
+    };
     describe('generateAccessToken', () => {
         it('Renvoi est une chaine non vide', async () => {
-            const accessToken = await generateAccessToken(userId);
+            const accessToken = await generateAccessToken(userPayload);
             expect(typeof accessToken).toBe('string');
         });
 
         it('Le token généré expire dans 15 minutes ', async () => {
-            const accessToken = await generateAccessToken(userId);
+            const accessToken = await generateAccessToken(userPayload);
             const decoded = jwt.decode(accessToken);
             const time = decoded?.exp - decoded?.iat!;
             expect(time).toBe(900); // 15minutes === 900 s
         });
 
         it('Le token contient bien l’identifiant utilisateur', async () => {
-            const accessToken = await generateAccessToken(userId);
+            const accessToken = await generateAccessToken(userPayload);
             const decoded = jwt.decode(accessToken);
-            expect(decoded?.userId).toBe(userId);
+            expect(decoded?.id).toBe(userPayload.id);
         });
 
         it('Deux tokens générés avec des appels différents sont bien différents', async () => {
             // iat l'heure à laquelle le token est emit
             // même si le token est emit avec le même userid l'heure d'emission est différente
 
-            const accessToken1 = await generateAccessToken(userId);
+            const accessToken1 = await generateAccessToken(userPayload);
             const t1 = jwt.decode(accessToken1)?.iat;
-            const accessToken2 = await generateAccessToken(userId);
+            const accessToken2 = await generateAccessToken(userPayload);
             const t2 = jwt.decode(accessToken2)?.iat;
 
             expect(t1).toBe(t2);
@@ -37,30 +43,30 @@ describe('Helpers de gestion des tokens', () => {
 
     describe('generateRefreshToken', () => {
         it('Renvoi est une chaine non vide', async () => {
-            const refreshToken = await generateRefreshToken(userId);
+            const refreshToken = await generateRefreshToken(userPayload);
             expect(typeof refreshToken).toBe('string');
         });
 
         it('Le token généré expire dans 24h ', async () => {
-            const refreshToken = await generateRefreshToken(userId);
+            const refreshToken = await generateRefreshToken(userPayload);
             const decoded = jwt.decode(refreshToken);
             const time = decoded?.exp - decoded?.iat!;
             expect(time).toBe(86400); // 24h === 86400 s
         });
 
         it('Le token contient bien l’identifiant utilisateur', async () => {
-            const refreshToken = await generateRefreshToken(userId);
+            const refreshToken = await generateRefreshToken(userPayload);
             const decoded = jwt.decode(refreshToken);
-            expect(decoded?.userId).toBe(userId);
+            expect(decoded?.id).toBe(userPayload.id);
         });
 
         it('Deux tokens générés avec des appels différents sont bien différents', async () => {
             // iat l'heure à laquelle le token est emit
             // même si le token est emit avec le même userid l'heure d'emission est différente
 
-            const refreshToken1 = await generateAccessToken(userId);
+            const refreshToken1 = await generateAccessToken(userPayload);
             const t1 = jwt.decode(refreshToken1)?.iat;
-            const refreshToken2 = await generateAccessToken(userId);
+            const refreshToken2 = await generateAccessToken(userPayload);
             const t2 = jwt.decode(refreshToken2)?.iat;
 
             expect(t1).toBe(t2);
